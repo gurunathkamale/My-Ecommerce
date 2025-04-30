50afd17a87b0b6e2e3bc705b96f3a9b27e8cf625
Kamale Gurunath <gurunathkamale123@gmail.com>
2025-04-30 19:39:56 +0530

    Update CardContact.js

--- src/components/CardContact.js
+++ src/components/CardContact.js
@@ -3,12 +3,7 @@ import React, { Fragment } from 'react'
 const CardContact = (props) => {
   return (
     <Fragment>
-    {/* <pre>{JSON.stringify(props.data)}</pre> */}
-    {/* <p>{props.data.name.first}</p> */}
-    {/* <div className='card text-center sticky-top'>
-    <div className='card-header bg-primary text-center'>
-      <div className='h4'/>
-      </div> */}
+  
    
     
     {
@@ -40,38 +35,7 @@ const CardContact = (props) => {
       </Fragment>
       : null
     }
-    {/* {
-        Object.keys(props.data).length > 0 ?
-        <Fragment>
-        <div className='card'>
-        <div className='card-header bg-primary'>
-            <div className='p4'/>
-        </div>
-        <div className='card-body text-center'>
-        <img src={props.data.picture} className='img-fluid img-thumbnail rounded-circle w-50 contact-img' alt=''/>
-        {/* <ul className='list-group text-left mt-3'>
-                                <li className='list-group-item list-group-item-primary'>
-                                    NAME: {props.data.name.first} {this.props.data.name.last}
-                                </li>
-                                <li className='list-group-item list-group-item-primary'>
-                                    AGE: {props.data.dob.age}
-                                </li>
-                                <li className='list-group-item list-group-item-primary'>
-                                    EMAIL: {props.data.email}
-                                </li>
-                                <li className='list-group-item list-group-item-primary'>
-                                    ADDRESS: {props.data.location.city}
-                                </li>
-                             </ul>
-        </div>
-      </div>
-      {/* </Fragment> : null */}
-    {/* }  */}
-        
-    {/* </div> */}
-    
-   
-      {/* <p>{props.data.name.first}</p> */}
+  
     </Fragment>
   )
 }
